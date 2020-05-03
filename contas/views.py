dd0804a679988cc4d7b728e93152b16818c59e30
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-05-03 16:16:03 -0300

    Implementados exemplo de uso da linguagem de template Python - JINJA

--- contas/views.py
+++ contas/views.py
@@ -7,4 +7,7 @@ def home(request):
     # now = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
     # return HttpResponse(html)
-    return render(request,'contas/home.html')
+    data = {}
+    data['transacoes'] = ['T1','T2','T3','T4']
+    data['now'] = datetime.datetime.now()
+    return render(request,'contas/home.html', data)
